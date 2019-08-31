07f54d06d2d1a3fafc8b8fb7c503f92633deff82
Agniv <agniv.others@gmail.com>
2019-08-31 14:32:37 +0530

    Started Chapter 3

---
+++ chapter3.py
@@ -0,0 +1,7 @@
+from bs4 import BeautifulSoup
+
+with open("ecologicalpyramid.html","r") as ecological_pyramid:
+  soup = BeautifulSoup(ecological_pyramid,"lxml")
+producer_entries = soup.find("ul")
+print(type(producer_entries))
+print(producer_entries.li.div.string)
