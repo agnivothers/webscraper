b22be50aa6b8fe729c76b7a8ddc3316536d2e1f8
Agniv <agniv.others@gmail.com>
2019-08-31 10:48:51 +0530

    Added some exceptions

--- webcraper.py
+++ webcraper.py
@@ -1,7 +1,20 @@
 from urllib.request import urlopen
+from urllib.error import HTTPError
+from urllib.error import URLError
 from bs4 import BeautifulSoup
 
+try:
+  #html = urlopen('http://pythonscraping.com/pages/page1.html')
+  html = urlopen('http://localhost')
 
-html = urlopen('http://pythonscraping.com/pages/page1.html')
-bs = BeautifulSoup(html.read(),'html.parser')
-print(bs.h1)
+except HTTPError as e:
+  print(e)
+except URLError as e:
+  print(e)
+else:
+  bs = BeautifulSoup(html.read(),'html.parser')
+try:
+  print(bs.nonExistantTag123.sometag)
+except AttributeError as e:
+  print(e)
+  print(bs.h1)
