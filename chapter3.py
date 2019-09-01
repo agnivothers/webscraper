d61fbc417a9343b902a19d4f79500f738e62a5d2
Agniv <agniv.others@gmail.com>
2019-09-01 10:24:11 +0530

    Now committing in the main local repo

--- chapter3.py
+++ chapter3.py
@@ -5,3 +5,15 @@ with open("ecologicalpyramid.html","r") as ecological_pyramid:
 producer_entries = soup.find("ul")
 print(type(producer_entries))
 print(producer_entries.li.div.string)
+primary = soup.find(id="primaryconsumers")
+print(primary.text)
+
+css_class = soup.find(attrs={'class':'primaryconsumerlist'})
+print(css_class)
+all_tertiaryconsumers = soup.find_all(class_="tertiaryconsumerlist")
+for tertiaryconsumer in all_tertiaryconsumers:
+  print(tertiaryconsumer.div.string)
+print("------------")
+all_li = soup.findAll("div",class_="name")
+for li in all_li:
+  print(li.string)
