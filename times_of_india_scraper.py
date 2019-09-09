ba20b655e7d4349889612df056a412c1cf19ea51
agniv <agniv@agniv-HP-406-G1-MT>
2019-09-09 18:19:42 +0530

    Getting top stories.

--- times_of_india_scraper.py
+++ times_of_india_scraper.py
@@ -5,4 +5,8 @@ from bs4 import BeautifulSoup
 url = "https://timesofindia.indiatimes.com/"
 html = urlopen(url)
 soup = BeautifulSoup(html,"lxml")
-print(soup)
+#print(soup)
+top_stories = soup.find("div",class_="top-story")
+print(top_stories)
+
+
